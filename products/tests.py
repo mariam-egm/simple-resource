6237c0485804c6b9aade1be84170c37f9eb04bfc
mariam-egm <mariam.gerges014@gmail.com>
2023-12-21 00:12:40 +0200

    test create product

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Product
 
-# Create your tests here.
+class ProductAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_product(self):
+        data = {'name': 'Test Product', 'description': 'This is a test product.'}
+        response = self.client.post('/resource/', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().name, 'Test Product')
