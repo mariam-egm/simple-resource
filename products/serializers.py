e2f6c39b82d5f2ec626e8204913ed1afb2eab4aa
mariam-egm <mariam.gerges014@gmail.com>
2023-12-21 00:04:27 +0200

    add validation to the name and description in serializer

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,23 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def validate_name(self, value):
+        """
+        Custom validation for the 'name' field.
+        Ensure that the name is not empty and does not exceed a certain length.
+        """
+        if not value:
+            raise serializers.ValidationError("Name cannot be empty.")
+        if len(value) > 100:
+            raise serializers.ValidationError("Name cannot exceed 100 characters.")
+        return value
+
+    def validate_description(self, value):
+        """
+        Custom validation for the 'description' field.
+        Ensure that the description does not exceed a certain length.
+        """
+        if len(value) > 500:
+            raise serializers.ValidationError("Description cannot exceed 500 characters.")
+        return value
